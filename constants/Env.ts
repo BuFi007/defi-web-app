9be205ab97fa5217143da64fe7ecf1f0fa9a2a58
Tomas Oliver <tomi204@outlook.com>
2024-11-21 18:46:20 -0300

    fix: env if fix

--- constants/Env.ts
+++ constants/Env.ts
@@ -1,25 +1,16 @@
-export const DYNAMIC_ENVIRONMENT_ID = 
+export const DYNAMIC_ENVIRONMENT_ID =
   process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID!;
 
 const MAINNET_URL = process.env.NEXT_PUBLIC_MAINNET_URL;
 
 const TESTNET_URL = process.env.NEXT_PUBLIC_TESTNET_URL;
 
-if (!DYNAMIC_ENVIRONMENT_ID) {
+if (!DYNAMIC_ENVIRONMENT_ID || !MAINNET_URL || !TESTNET_URL) {
   throw new Error("NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID is not set");
 }
 
-if (!MAINNET_URL) {
-  throw new Error("NEXT_PUBLIC_MAINNET_URL is not set");
-}
-
-if (!TESTNET_URL) {
-  throw new Error("NEXT_PUBLIC_TESTNET_URL is not set");
-}
-
-export const IS_MAINNET = typeof window !== 'undefined' 
-  ? Boolean(MAINNET_URL && window.location.href.includes(MAINNET_URL)) || 
-    Boolean(TESTNET_URL && window.location.href.includes(TESTNET_URL))
-  : false;
-
-
+export const IS_MAINNET =
+  typeof window !== "undefined"
+    ? Boolean(MAINNET_URL && window.location.href.includes(MAINNET_URL)) ||
+      Boolean(TESTNET_URL && window.location.href.includes(TESTNET_URL))
+    : false;
