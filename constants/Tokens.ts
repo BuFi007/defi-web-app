83dceea4faf665f82072146fa2f89219e7339bee
Tomas Oliver <tomi204@outlook.com>
2024-12-15 06:48:10 -0300

    feat: added testnet and change in hooks

--- constants/Tokens.ts
+++ constants/Tokens.ts
@@ -127,7 +127,7 @@ export const ArbitrumTokens: Token[] = [
 export const ArbitrumSepoliaTokens: Token[] = [
   {
     address: NATIVE_TOKEN_ADDRESS,
-    chainId: 421614,
+    chainId: 11155420,
     decimals: 18,
     name: "Ethereum",
     payable: false,
@@ -137,7 +137,7 @@ export const ArbitrumSepoliaTokens: Token[] = [
   },
   {
     address: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d" as Hex,
-    chainId: 421614,
+    chainId: 11155420,
     decimals: 6,
     name: "USDC",
     payable: true,
@@ -184,6 +184,29 @@ export const OptimismTokens: Token[] = [
   {
     address: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85" as Hex,
     chainId: 10,
+    decimals: 6,
+    name: "USDC",
+    payable: true,
+    symbol: "USDC",
+    image:
+      "https://dynamic-assets.coinbase.com/3c15df5e2ac7d4abbe9499ed9335041f00c620f28e8de2f93474a9f432058742cdf4674bd43f309e69778a26969372310135be97eb183d91c492154176d455b8/asset_icons/9d67b728b6c8f457717154b3a35f9ddc702eae7e76c4684ee39302c4d7fd0bb8.png",
+  },
+];
+
+export const SepoliaOptimismTokens: Token[] = [
+  {
+    address: NATIVE_TOKEN_ADDRESS,
+    chainId: 11155111,
+    decimals: 18,
+    name: "Ethereum",
+    payable: false,
+    symbol: "ETH",
+    image:
+      "https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png",
+  },
+  {
+    address: "0x5fd84259d66Cd46123540766Be93DFE6D43130D7" as Hex,
+    chainId: 11155111,
     decimals: 6,
     name: "USDC",
     payable: true,
@@ -247,6 +270,7 @@ export const allTokens = [
   ...ArbitrumTokens,
   ...ArbitrumSepoliaTokens,
   ...BscTokens,
+  ...SepoliaOptimismTokens,
   ...OptimismTokens,
   ...ZkSyncTokens,
   ...ZkSyncSepoliaTokens,
